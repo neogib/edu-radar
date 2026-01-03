8b0064638625b4ec06bd04d15c431ac5b5633573
Gibirizon <swojteksz@gmail.com>
2026-01-03 21:44:20 +0100

    feat: add creating tables on startup

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,20 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.app.core.database import create_db_and_tables
 from src.app.routers import school_types, schools
 
-app = FastAPI()
+
+@asynccontextmanager
+async def on_startup(_: FastAPI):
+    # Initialize database and create tables if they don't exist on start of the app
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=on_startup)
 
 
 origins = [
