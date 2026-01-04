a83dd76354525e09d34677b0fdc3cfb2a9e571dd
Gibirizon <swojteksz@gmail.com>
2026-01-04 12:42:07 +0100

    fix: remove lifespan which is not needed any more

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,20 +1,9 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.app.core.database import create_db_and_tables
 from src.app.routers import school_types, schools
 
-
-@asynccontextmanager
-async def on_startup(_: FastAPI):
-    # Initialize database and create tables if they don't exist on start of the app
-    create_db_and_tables()
-    yield
-
-
-app = FastAPI(lifespan=on_startup)
+app = FastAPI()
 
 
 origins = [
