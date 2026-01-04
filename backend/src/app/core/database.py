ea3f5a9bcee4420e9fb70031b5f3d2cab2516311
Gibirizon <swojteksz@gmail.com>
2026-01-04 14:36:11 +0100

    feat: new migration to add composite index on lat, lon

--- backend/src/app/core/database.py
+++ backend/src/app/core/database.py
@@ -4,11 +4,11 @@ from src.app.core.config import Settings
 
 # DATABASE_URI is of type PostgresDsn, that's why we need get_connection_string method
 settings = Settings()  # pyright: ignore[reportCallIssue]
-engine = create_engine(settings.get_connection_string(), echo=True)
+engine = create_engine(settings.get_connection_string())
 
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
 
 
 def get_session():
