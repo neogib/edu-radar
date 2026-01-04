2f83113e218e2b682bb52b2a4a88c71e0f927430
Gibirizon <swojteksz@gmail.com>
2026-01-04 14:39:00 +0100

    feat: remove unnecessary geocoder

--- backend/src/data_import/geo/exceptions.py
+++
@@ -1,8 +0,0 @@
-class GeocodingError(Exception):
-    """Raised when geocoding fails."""
-
-    address: str
-
-    def __init__(self, message: str, address: str):
-        self.address = address
-        super().__init__(f"{message} (address={address!r})")
