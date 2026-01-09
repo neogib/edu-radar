93993d2b29eac219b1fd781f0a35ddf6c9c123b1
Gibirizon <swojteksz@gmail.com>
2026-01-09 09:35:23 +0100

    feat: add pinia

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-maplibre"],
+    modules: ["@nuxt/eslint", "@nuxt/ui", "nuxt-maplibre", "@pinia/nuxt"],
     // Runtime environment configuration
     runtimeConfig: {
         proxyURL: "http://localhost:8000",
