0ec0a870cc316fa219c729daa28ff472d186c9e5
neogib <swojteksz@gmail.com>
2026-02-23 17:05:53 +0100

    refactor(backend): remove not needed CORSMiddleware
    - all traffic from frontend goes through Nuxt proxy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.exception_handlers import register_exception_handlers
 from app.api.v1.router import api_v1_router
@@ -10,16 +9,6 @@ configure_logging()
 app = FastAPI()
 register_exception_handlers(app)
 app.include_router(api_v1_router, prefix="/api/v1")
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/test")
