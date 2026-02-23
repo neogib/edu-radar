73b2de883417696c6347fa658b4f27ed7f03d7d4
neogib <swojteksz@gmail.com>
2026-02-23 18:11:01 +0100

    feat(frontend): update security settings for dev and prod

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,7 @@
 import tailwindcss from "@tailwindcss/vite"
+
+const isDev = process.env.NODE_ENV === "development"
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
@@ -30,6 +33,52 @@ export default defineNuxtConfig({
     },
     devServer: {
         host: "0.0.0.0",
+    },
+    security: {
+        headers: {
+            strictTransportSecurity: isDev
+                ? false
+                : {
+                      maxAge: 15_552_000,
+                      includeSubdomains: true,
+                  },
+            crossOriginEmbedderPolicy: isDev ? "unsafe-none" : "require-corp",
+            contentSecurityPolicy: isDev
+                ? {
+                      "upgrade-insecure-requests": false,
+                      "script-src": [
+                          "'self'",
+                          "'unsafe-inline'",
+                          "'unsafe-eval'",
+                          "http:",
+                          "https:",
+                      ],
+                      "style-src": ["'self'", "'unsafe-inline'", "https:"],
+                      "connect-src": [
+                          "'self'",
+                          "http:",
+                          "https:",
+                          "ws:",
+                          "wss:",
+                      ],
+                  }
+                : {
+                      "script-src": [
+                          "'self'",
+                          "https:",
+                          "'unsafe-inline'",
+                          "'strict-dynamic'",
+                          "'nonce-{{nonce}}'",
+                      ],
+                      "style-src": ["'self'", "https:", "'unsafe-inline'"],
+                      "img-src": ["'self'", "data:", "blob:"],
+                      "font-src": ["'self'", "https:", "data:"],
+                      "object-src": ["'none'"],
+                      "base-uri": ["'none'"],
+                      "script-src-attr": ["'none'"],
+                      "upgrade-insecure-requests": true,
+                  },
+        },
     },
     app: {
         head: {
